3dc546f657380272017d51a547222e2a2adac5b5
RUSHi-VAiRALE <rushivairale932@gmail.com>
2025-03-29 18:38:45 +0530

    added new image

--- src/components/about/TeamSection.jsx
+++ src/components/about/TeamSection.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import ceoImage from '../../../public/alok.jpg'
-import ctoImage from '../../../public/nikhil.jpg'
+import ctoImage from '../../../public/nikhil.png'
 import { fadeIn, staggerContainer } from './AnimationVariants';
 
 const TeamSection = () => {
